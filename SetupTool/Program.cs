82e32e51a2964a8e4e11075c31a6572e3032c390
Human Gamer <39096122+HumanGamer@users.noreply.github.com>
2020-12-13 14:27:46 -0600

    Added Missing UpdateSteamDirTargetsFile Method

--- SetupTool/Program.cs
+++ SetupTool/Program.cs
@@ -16,6 +16,28 @@ namespace SetupTool
 				new DecompileTask(new ConsoleTaskCallback(), Defines.ProjectConfig.DecompiledSrcDir),
 				new ProcessProjectTask(new ConsoleTaskCallback(), Defines.ProjectConfig.Projects)
 			));
+
+			UpdateSteamDirTargetsFile();
+		}
+
+		private static readonly string targetsFilePath = Path.Combine(Defines.ProjectConfig.SrcDir, "TerrariaSteamPath.targets");
+		private static void UpdateSteamDirTargetsFile()
+		{
+			BaseTask.CreateParentDirectory(targetsFilePath);
+
+			string targetsText =
+$@"<?xml version=""1.0"" encoding=""utf-8""?>
+<Project ToolsVersion=""14.0"" xmlns=""http://schemas.microsoft.com/developer/msbuild/2003"">
+  <PropertyGroup>
+    <TerrariaSteamPath>{Defines.SteamDir}</TerrariaSteamPath>
+  </PropertyGroup>
+</Project>";
+
+
+			if (File.Exists(targetsFilePath) && targetsText == File.ReadAllText(targetsFilePath))
+				return;
+
+			File.WriteAllText(targetsFilePath, targetsText);
 		}
 
 		public class ConsoleTaskCallback : ITaskInterface
